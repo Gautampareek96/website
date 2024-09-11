2b4cf45ec4d598fcb6174f48e1dd3f2eeed0fab3
Gautampareek96 <69616811+Gautampareek96@users.noreply.github.com>
2024-09-11 07:13:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+/*const slides = document.querySelectorAll(".slide img");
+let slideIndex = 0;
+let intervalId = null;
+
+initializeSlider();
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+function showSlide(index){
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length -1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide")
+    });
+    slides[slideIndex].classList.add("displaySlide")
+}
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+function nextSlide(){
+     slideIndex++;
+     showSlide(slideIndex);
+}*/
+
+const slides = document.querySelectorAll(".slide");
+let slideIndex = 0;
+let intervalId = null;
+
+initializeSlider();
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+        hideOtherSlides(slideIndex);
+    }
+}
+
+function hideOtherSlides(index) {
+    slides.forEach((slide, i) => {
+        if (i !== index) {
+            slide.classList.add("hideSlide");
+        } else {
+            slide.classList.remove("hideSlide");
+        }
+    });
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length -1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+    hideOtherSlides(slideIndex);
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
